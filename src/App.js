33fbe575bac84ea9f18066b9a5605cfb316d9b08
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-12-13 09:36:16 +0100

    Integración de bootstrap en eact
    Como ejemplo se ha componentizado el menú denavegación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hola mundo, estoy usando React 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navigation />
+        <div className="container">
+          <h1>My Storeroom React App</h1>
+        </div>
       </div>
     );
   }

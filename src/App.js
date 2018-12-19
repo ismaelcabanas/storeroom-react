7ee12b3bc4c137bc6b5f5edeb86150e8ad506a25
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-12-19 09:28:39 +0100

    Trabajando con listas en React
    En este commit se transforma cada elemento del array en un componente de React. Pero, cuando ejecutamos el código vemosque en consola nos sale un error indicando que cada elemento del array debería tener una pro key que identifique unívocamente cada elemento del array.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
-import ConditionalSection from './sections/Conditional';
 
 class App extends Component {
   render() {
+    const numbers = [1, 1, 3, 4, 5]
+
     return (
       <div>
         <Navigation />
-        <div className="container">
-          <ConditionalSection />
-        </div>
+        {numbers.map(number => {
+          return <p>I am the number {number}</p>
+        })}
       </div>
     );
   }

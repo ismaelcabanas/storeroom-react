1aed2752b9a8af842e05d2a6284baa712ff902ef
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-12-19 09:14:53 +0100

    Uso de variables para guardar funciones React
    En React, podemos almacenar el resultado de una función un componente de React, y usarlo posteriormente.

--- src/sections/Conditional.js
+++ src/sections/Conditional.js
@@ -25,10 +25,11 @@ export default class ConditionalSection extends Component{
         this.state = {mostrarA: true}
     }
     render () {
+        const conditionalComponent = useConditionalRendering(this.state.mostrarA)
         return (
             <div>
                 <h4>Conditional Section</h4>
-                {useConditionalRendering(this.state.mostrarA)}
+                {conditionalComponent}
             </div>
         )
     }

5842db7f881c7553a9875a8e6054475eaa812838
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-12-27 00:09:04 +0100

    Usando eventos
    En este ejemplo se muestra el uso de un evento de rató para gestionar el estado del componente,  y se crea una función javascript que gestion el evento. Pero, cuando ejecutamos el código y pasamos el rtón por encima del componente vemos que da un error. al tratar establecer el nuesto state del componente, ya que el contexto this de la función no es el contexto del componente.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {mouseX: 0, mouseY: 0}
+  }
+
+  handleMouseMove(e) {
+    const {clientX, clientY} = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+
   handleClick(e) {
     console.log(e)
     console.log(e.nativeEvent)
@@ -15,6 +26,11 @@ class App extends Component {
         <div className="container">
           <h4>Events</h4>
           <button onClick={this.handleClick}>Hi there!</button>
+        </div>      
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{border: '1px solid #000', marginTop: 10, padding: 10}}>
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
         </div>        
       </div>
     );

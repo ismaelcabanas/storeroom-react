52032958e0f3fa495d92d28e9595d440948b4efc
Ismael Cabañas García <ismaelcabanas@gmail.com>
2019-01-03 09:36:10 +0100

    New unit tests for App container component

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from '../../containers/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", function() {
+
+  it('renders without crashing', () => {
+    let mountedApp = shallow(<App />);  
+  });
+
+  it('renders menu navigation', () => {
+    let mountedApp = shallow(<App />);
+    const navigation = mountedApp.find('Navigation');
+    expect(navigation.length).toBe(1);
+  });
+
+  it('renders add storeroom form', () => {
+    let mountedApp = shallow(<App />);
+    const storeroom = mountedApp.find('AddStoreroom');
+    expect(storeroom.length).toBe(1);
+  });
 });
+
